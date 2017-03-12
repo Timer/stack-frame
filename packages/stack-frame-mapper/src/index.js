20f4b56f2df03773206149997229db3f4a9e9ce2
Joe Haddad <timer150@gmail.com>
2017-03-12 16:30:57 -0400

    Mapper only requires frames

--- packages/stack-frame-mapper/src/index.js
+++ packages/stack-frame-mapper/src/index.js
@@ -2,34 +2,36 @@
 import StackFrame from 'stack-frame';
 import getSourceMap from 'stack-frame-utils/lib/getSourceMap';
 
-async function map(
-  frames: StackFrame[],
-  fileUri: string,
-  fileContents: ?string
-): Promise<StackFrame[]> {
-  if (fileContents == null) {
-    fileContents = await fetch(fileUri).then(res => res.text());
-  }
-  const map = await getSourceMap(fileUri, fileContents);
-  return frames.map(frame => {
-    const { functionName, fileName, lineNumber, columnNumber } = frame;
-    const { source, line, column } = map.originalPositionFor({
-      line: lineNumber,
-      column: columnNumber,
-    });
-    return new StackFrame(
-      functionName,
-      fileName,
-      lineNumber,
-      columnNumber,
-      undefined,
-      functionName,
-      source,
-      line,
-      column,
-      undefined
-    );
-  });
+async function map(frames: StackFrame[]): Promise<StackFrame[]> {
+  const cache = {};
+  return await Promise.all(
+    frames.map(async frame => {
+      const { functionName, fileName, lineNumber, columnNumber } = frame;
+      let map = cache[fileName];
+      if (map == null) {
+        map[fileName] = await getSourceMap(
+          fileName,
+          await fetch(fileName).then(r => r.text())
+        );
+      }
+      const { source, line, column } = map.originalPositionFor({
+        line: lineNumber,
+        column: columnNumber,
+      });
+      return new StackFrame(
+        functionName,
+        fileName,
+        lineNumber,
+        columnNumber,
+        undefined,
+        functionName,
+        source,
+        line,
+        column,
+        undefined
+      );
+    })
+  );
 }
 
 export { map };
