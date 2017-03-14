2b78dea8226e69c4b3f30a2d9105a58320dde53a
Joe Haddad <timer150@gmail.com>
2017-03-13 22:57:36 -0400

    Major speedup (actual file caching)

--- packages/stack-frame-mapper/src/index.js
+++ packages/stack-frame-mapper/src/index.js
@@ -1,6 +1,7 @@
 //@flow
 import StackFrame from 'stack-frame';
 import { getSourceMap, getLinesAround } from 'stack-frame-utils';
+import { settle } from 'settle-promise';
 
 /**
  * Enhances a set of <code>{@link https://github.com/Timer/stack-frame/tree/master/packages/stack-frame#stackframe StackFrame}</code>s with their original positions and code (when available).
@@ -8,34 +9,43 @@ import { getSourceMap, getLinesAround } from 'stack-frame-utils';
  */
 async function map(frames: StackFrame[]): Promise<StackFrame[]> {
   const cache = {};
-  return await Promise.all(
-    frames.map(async frame => {
-      const { functionName, fileName, lineNumber, columnNumber } = frame;
-      let { map, fileSource } = cache[fileName] || {};
-      if (map == null) {
-        fileSource = await fetch(fileName).then(r => r.text());
-        map = await getSourceMap(fileName, fileSource);
-        cache[fileName] = { map, fileSource };
-      }
-      const { source, line, column } = map.getOriginalPosition(
-        lineNumber,
-        columnNumber
-      );
-      const originalSource = source == null ? [] : map.getSource(source);
-      return new StackFrame(
-        functionName,
-        fileName,
-        lineNumber,
-        columnNumber,
-        getLinesAround(lineNumber, 3, fileSource),
-        functionName,
-        source,
-        line,
-        column,
-        getLinesAround(line, 3, originalSource)
-      );
+  const files = [];
+  frames.forEach(frame => {
+    const { fileName } = frame;
+    if (files.indexOf(fileName) !== -1) return;
+    files.push(fileName);
+  });
+  await settle(
+    files.map(async fileName => {
+      const fileSource = await fetch(fileName).then(r => r.text());
+      const map = await getSourceMap(fileName, fileSource);
+      cache[fileName] = { fileSource, map };
     })
   );
+  return frames.map(frame => {
+    const { functionName, fileName, lineNumber, columnNumber } = frame;
+    let { map, fileSource } = cache[fileName] || {};
+    if (map == null) {
+      return frame;
+    }
+    const { source, line, column } = map.getOriginalPosition(
+      lineNumber,
+      columnNumber
+    );
+    const originalSource = source == null ? [] : map.getSource(source);
+    return new StackFrame(
+      functionName,
+      fileName,
+      lineNumber,
+      columnNumber,
+      getLinesAround(lineNumber, 3, fileSource),
+      functionName,
+      source,
+      line,
+      column,
+      getLinesAround(line, 3, originalSource)
+    );
+  });
 }
 
 export { map };
