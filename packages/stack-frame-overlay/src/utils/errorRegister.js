1a626907fba89bad5dce73bf0f58451939dc7f87
Joe Haddad <timer150@gmail.com>
2017-03-19 21:32:22 -0400

    Async code is unnecessary

--- packages/stack-frame-overlay/src/utils/errorRegister.js
+++ packages/stack-frame-overlay/src/utils/errorRegister.js
@@ -2,6 +2,7 @@
 import type { StackFrame } from 'stack-frame';
 import { parse } from 'stack-frame-parser';
 import { map } from 'stack-frame-mapper';
+import { unmap } from 'stack-frame-unmapper';
 
 type ErrorRecord = {
   error: Error,
@@ -14,25 +15,36 @@ const recorded: ErrorRecord[] = [];
 
 let errorsConsumed: ErrorRecordReference = 0;
 
-async function consume(
+function consume(
   error: Error,
   unhandledRejection: boolean = false,
   contextSize: number = 3
 ): Promise<ErrorRecordReference> {
   const parsedFrames = parse(error);
-  let enhancedFrames = await map(parsedFrames, contextSize);
-  enhancedFrames = enhancedFrames.filter(
-    ({ functionName }) =>
-      functionName == null ||
-      functionName.indexOf('__stack_frame_overlay_proxy_console__') === -1
-  );
-  recorded[++errorsConsumed] = {
-    error,
-    unhandledRejection,
-    contextSize,
-    enhancedFrames,
-  };
-  return errorsConsumed;
+  let enhancedFramesPromise;
+  if (error.__unmap_source) {
+    enhancedFramesPromise = unmap(
+      error.__unmap_source,
+      parsedFrames,
+      contextSize
+    );
+  } else {
+    enhancedFramesPromise = map(parsedFrames, contextSize);
+  }
+  return enhancedFramesPromise.then(enhancedFrames => {
+    enhancedFrames = enhancedFrames.filter(
+      ({ functionName }) =>
+        functionName == null ||
+        functionName.indexOf('__stack_frame_overlay_proxy_console__') === -1
+    );
+    recorded[++errorsConsumed] = {
+      error,
+      unhandledRejection,
+      contextSize,
+      enhancedFrames,
+    };
+    return errorsConsumed;
+  });
 }
 
 function getErrorRecord(ref: ErrorRecordReference): ErrorRecord {
